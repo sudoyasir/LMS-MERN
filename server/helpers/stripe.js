e035043695ccb82714f2f5d4a74396bbf5724d8b
Yasir Nawaz <y451rmahar@gmail.com>
2025-01-22 16:41:13 +0500

    :rocket: initial commit

---
+++ server/helpers/stripe.js
@@ -0,0 +1,4 @@
+const Stripe = require('stripe');
+const stripe = Stripe("sk_test_51P856lAS9QcTsHP74v9i3iSPXVaQUd8fC10FXdTltuX1jMJnCrwurxaFbzQ56I20YKS6snTGCxpNdI8I4jmwSSjn00olIlTvfA");
+
+module.exports = stripe;
---
+++ server/server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth-routes/index");
+const mediaRoutes = require("./routes/instructor-routes/media-routes");
+const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
+const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
+const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
+const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
+
+
+
+const app = express();
+const PORT = 5000;
+const DB_NAME = "my_database"; // Replace with your desired database name
+const MONGO_URI = `mongodb+srv://yasir2002:yasir2002@cluster0.vgcb7fd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+//database connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb is connected"))
+  .catch((e) => console.log(e));
+
+//routes configuration
+app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
+app.use("/student/course", studentViewCourseRoutes);
+app.use("/student/order", studentViewOrderRoutes);
+app.use("/student/courses-bought", studentCoursesRoutes);
+app.use("/student/course-progress", studentCourseProgressRoutes);
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
